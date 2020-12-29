321b7a8dca1dbb1cf0232812ccccb43a268f7394
Hackerl <490021209@qq.com>
2020-12-29 18:15:39 +0800

    Add#pass source code by file

--- main.cpp
+++ main.cpp
@@ -16,11 +16,24 @@ constexpr auto PYTHON_LIBRARY = "libpython";
 int main(int argc, char ** argv) {
     cmdline::parser parse;
 
-    parse.add<std::string>("source", 's', "python source file", true, "");
+    parse.add<std::string>("source", 's', "python source code", true, "");
+    parse.add("file", '\0', "pass source by file");
+
     parse.parse_check(argc, argv);
 
     auto pid = getpid();
     auto source = parse.get<std::string>("source");
+
+    if (parse.exist("file")) {
+        std::ifstream ifs(source);
+
+        if (!ifs.is_open()) {
+            LOG_ERROR("open source file failed: %s", source.c_str());
+            return -1;
+        }
+
+        source = std::string(std::istreambuf_iterator<char>(ifs), std::istreambuf_iterator<char>());
+    }
 
     CProcessMap processMap;
 
@@ -28,17 +41,17 @@ int main(int argc, char ** argv) {
             !CProcess::getFileMemoryBase(pid, PYTHON_LIBRARY, processMap) &&
             !CProcess::getFileMemoryBase(pid, PYTHON, processMap)
             ) {
-        LOG_ERROR("find target library failed");
-        return 0;
+        LOG_ERROR("find target failed");
+        return -1;
     }
 
-    LOG_INFO("find target library: 0x%lx -> %s", processMap.start, processMap.file.c_str());
+    LOG_INFO("find target: 0x%lx -> %s", processMap.start, processMap.file.c_str());
 
     ELFIO::elfio reader;
 
     if (!reader.load(processMap.file)) {
         LOG_ERROR("open elf failed: %s", processMap.file.c_str());
-        return 0;
+        return -1;
     }
 
     auto it = std::find_if(
@@ -50,7 +63,7 @@ int main(int argc, char ** argv) {
 
     if (it == reader.sections.end()) {
         LOG_ERROR("can't find symbol section");
-        return 0;
+        return -1;
     }
 
     unsigned baseAddress = reader.get_type() == ET_EXEC ? 0 : processMap.start;
@@ -72,7 +85,7 @@ int main(int argc, char ** argv) {
 
         if (!symbols.get_symbol(i, name, value, size, bind, type, section,other)) {
             LOG_ERROR("get symbol %lu failed", i);
-            return 0;
+            return -1;
         }
 
         if (name == "PyGILState_Ensure")
@@ -84,8 +97,8 @@ int main(int argc, char ** argv) {
     }
 
     if (!pfnEnsure || !pfnRun || !pfnRelease) {
-        LOG_ERROR("can't find python functions");
-        return 0;
+        LOG_ERROR("can't find python symbols");
+        return -1;
     }
 
     LOG_INFO("ensure func: %p run func: %p release func: %p", pfnEnsure, pfnRun, pfnRelease);
@@ -95,19 +108,19 @@ int main(int argc, char ** argv) {
 
     if (!FS) {
         LOG_ERROR("get fs environment variable failed");
-        return 0;
+        return -1;
     }
 
     if (!CStringHelper::toNumber(FS, fs, 16)) {
-        LOG_ERROR("parse environment variable failed");
-        return 0;
+        LOG_ERROR("parse fs environment variable failed");
+        return -1;
     }
 
     LOG_INFO("set fs: 0x%lx", fs);
 
     if (do_syscall(SYS_arch_prctl, ARCH_SET_FS, fs) != 0) {
         LOG_ERROR("set fs failed");
-        return 0;
+        return -1;
     }
 
     int state = pfnEnsure();
